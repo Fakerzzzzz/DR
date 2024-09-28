67599ecc367a11c9a1e3f436f905c64a8a3af3b8
MuhammadAdib <107087988+Fakerzzzzz@users.noreply.github.com>
2024-09-28 11:39:19 +0800

    app.py

--- app.py
+++ app.py
@@ -1,8 +1,17 @@
 from flask import Flask, request, jsonify
 import numpy as np
 from joblib import load
+import os
 
 app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return "Hello, World!"
+
+if __name__ == '__main__':
+    port = int(os.environ.get('PORT', 5000))  # Use the PORT environment variable or default to 5000
+    app.run(host='0.0.0.0', port=port)
 
 # Load pre-trained models
 lr_model = load('lr_model.pkl')
